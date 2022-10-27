75f3077e8d3d3bd772f1bd338858f2815d8780b7
RuslanNezhenets <ruslaneshenets@gmail.com>
2022-10-27 10:04:21 +0300

    added eslint plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,14 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ['standard', 'plugin:sonarjs/recommended', 'prettier'],
+  extends: [
+    'standard',
+    'plugin:sonarjs/recommended',
+    'plugin:n/recommended',
+    'plugin:promise/recommended',
+    'plugin:import/recommended',
+    'prettier',
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
