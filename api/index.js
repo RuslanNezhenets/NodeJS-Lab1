061dd7bd8f75aa4160892e09c032863eaa93048d
RuslanNezhenets <ruslaneshenets@gmail.com>
2022-11-20 22:23:30 +0200

    connecting routers

--- api/index.js
+++ api/index.js
@@ -1,17 +1,14 @@
-import Router from '../lib/Router.js'
+import helloRouter from '../src/hello-router.js'
+import testRouter from '../src/test-router.js'
+import userRouter from '../src/user-router.js'
 import Application from '../lib/Application.js'
-
 const PORT = process.env.PORT || 5000
 
 const app = new Application()
 
-const router = new Router()
-
-router.get('/test', (req, res) => {
-  res.end('You send request to /test')
-})
-
-app.addRouter(router)
+app.addRouter(helloRouter)
+app.addRouter(testRouter)
+app.addRouter(userRouter)
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
 
